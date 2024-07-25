a1ae6ca4fa9477859ce868156ee9b2d1b128fa9c
Wai Thura Tun <maxw52493@gmail.com>
2024-07-25 21:39:07 +0700

    Initial Commit

---
+++ FloatButton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FloatButton
+//
+//  Created by Wai Thura Tun on 7/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
